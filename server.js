c9adcb3376c79c98c9a15dac1f6fa6c2e8b31f46
Sourav Gain <souravgain605@gmail.com>
2021-05-22 12:07:56 +0530

    Initial Express Setup

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server Listening on port ${port}`);
+});
