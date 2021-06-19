def14821358372085fce66b2b5bb244c35fe0398
Sourav Gain <souravgain605@gmail.com>
2021-06-19 13:50:43 +0530

    added common booking constrains handler

---
+++ utils/index.js
@@ -0,0 +1,32 @@
+const { NOTFOUND } = require("./error");
+
+const getBookingConstrains = (body) => {
+  const self_booking = body?.self_booking ? body.self_booking : false;
+
+  if (!self_booking) {
+    if (!body.name || typeof body.name != "string") {
+      throw new NOTFOUND("Patient Name (name)");
+    }
+
+    if (!body.age || typeof body.age != "number") {
+      throw new NOTFOUND("Patient Age (age)");
+    }
+
+    if (!body.mobile_no || typeof body.mobile_no != "number") {
+      throw new NOTFOUND("Patient Age (mobile_no)");
+    }
+  }
+
+  return !self_booking
+    ? {
+        patient_details: {
+          name: body.name,
+          age: body.age,
+          mobile_no: body.mobile_no,
+        },
+        self_booking: false,
+      }
+    : { self_booking: true };
+};
+
+module.exports = { getBookingConstrains };
